5b79d20c2b7e5abd0ad4906aa9cc0ca3845e4d66
王亮 <liang@edison.tech>
2019-11-07 17:41:02 +0800

    fix: add to group, no options below search box (show empty)

--- app/internal_packages/edison-beijing-chat/components/chat/new/InviteGroupChatList.jsx
+++ app/internal_packages/edison-beijing-chat/components/chat/new/InviteGroupChatList.jsx
@@ -2,30 +2,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ContactAvatar from '../../common/ContactAvatar';
 import Select, { Option } from 'rc-select';
-
 export default class InviteGroupChatList extends Component {
   static propTypes = {
     onContactClicked: PropTypes.func,
     groupMode: PropTypes.bool,
-    contacts: PropTypes.arrayOf(PropTypes.shape({
-      jid: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      email: PropTypes.string,
-      avatar: PropTypes.string
-    })),
-    selectedContacts: PropTypes.arrayOf(PropTypes.shape({
-      jid: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      avatar: PropTypes.string
-    })),
-  }
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        jid: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string,
+        avatar: PropTypes.string,
+      })
+    ),
+    selectedContacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        jid: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        avatar: PropTypes.string,
+      })
+    ),
+  };
 
   static defaultProps = {
     groupMode: false,
     contacts: [],
     selectedContacts: [],
-  }
+  };
 
   constructor() {
     super();
@@ -41,39 +44,47 @@ export default class InviteGroupChatList extends Component {
   onUpdateGroup = (value, option) => {
     this.setState({ selectedContacts: [], inviting: false, open: false });
     this.props.onUpdateGroup([option.props.contact]);
-  }
+  };
 
   render() {
     const { open } = this.state;
     const { contacts } = this.props;
-    const children = open ? contacts.map(contact =>
-      <Option
-        key={contact.jid}
-        jid={contact.jid}
-        curjid={contact.curJid}
-        value={contact.name + contact.email}
-        email={contact.email}
-        label={contact.name}
-        contact={contact}
-        className="invite-contact-option"
-      >
-        <div className="chip">
-          <ContactAvatar jid={contact.jid} name={contact.name}
-            email={contact.email} size={32} />
-          <span className="contact-name">{contact.name}</span>
-          <span className="contact-email">{contact.email}</span>
-        </div>
-      </Option>
-    ) : [];
+    const children = open
+      ? contacts.map(contact => (
+          <Option
+            key={contact.jid}
+            jid={contact.jid}
+            curjid={contact.curJid}
+            value={contact.name + contact.email}
+            email={contact.email}
+            label={contact.name}
+            contact={contact}
+            className="invite-contact-option"
+          >
+            <div className="chip">
+              <ContactAvatar
+                jid={contact.jid}
+                name={contact.name}
+                email={contact.email}
+                size={32}
+              />
+              <span className="contact-name">{contact.name}</span>
+              <span className="contact-email">{contact.email}</span>
+            </div>
+          </Option>
+        ))
+      : [];
 
     return (
       <div className="contactsList">
         <h2>Add to Group...</h2>
-        <div style={{ flex: 1 }}>
+        <div>
           <Select
             ref="contacts"
             mode="tags"
-            style={{ width: '100%', flex: 1 }}
+            style={{ width: '100%' }}
+            dropdownStyle={{ top: 156, height: 400 }}
+            className="contactsListSelect"
             onSelect={this.onUpdateGroup}
             open={open}
             defaultOpen={true}
