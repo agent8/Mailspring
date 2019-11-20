84d8c6886bb0e5090d996f9d8db58caf3915cb0f
Xingming Cao <xingming@edison.tech>
2019-11-20 09:40:04 +0800

    use className for TeamreplyEditor

--- app/internal_packages/composer/lib/TeamreplyEditor.jsx
+++ app/internal_packages/composer/lib/TeamreplyEditor.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
 
-export default class ComposerEditor extends Component {
+export default class TeamreplyEditor extends Component {
   constructor (props) {
     super(props)
   }
   render () {
-    return (
-      <webview
-        src='http://0.0.0.0:8080/p/52316330ebe411e9a7be1b757c407294'
-        style={{ display: 'inline-flex', width: '100%', height: '350px' }}
-      />
-    )
+    return <iframe className='teamreply-editor' src='http://0.0.0.0:8080/p/52316330ebe411e9a7be1b757c407294' />
   }
 }
