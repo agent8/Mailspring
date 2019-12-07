0bf0800a995a50e7e5d9f3c73766fb653299d484
quanzs <quanzhansheng@gmail.com>
2019-12-07 14:23:46 +0800

    invite beta flow

--- app/internal_packages/onboarding/lib/page-login.jsx
+++ app/internal_packages/onboarding/lib/page-login.jsx
@@ -20,7 +20,11 @@ export default class LoginPage extends React.Component {
     OnboardingActions.moveToPage('account-choose');
     setTimeout(() => {
       const win = AppEnv.getCurrentWindow();
-      win.unmaximize();
+      // win.unmaximize();
+      win.setSize(700, 685);
+      const left = Math.floor((screen.width - 700) / 2);
+      const top = Math.floor((screen.height - 685) / 2 - 30);
+      win.setPosition(left, top);
       win.setResizable(false);
     }, 10)
   };
