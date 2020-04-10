c16b3c9db7e774624c039eaded85b9a005957d88
ruoxijiang <rjiang@lazycakestudios.net>
2020-04-10 18:08:54 +0800

    DC-1622 Attachment icon for thread not longer shown

--- app/internal_packages/thread-list/lib/thread-list-columns.jsx
+++ app/internal_packages/thread-list/lib/thread-list-columns.jsx
@@ -15,7 +15,7 @@ const ThreadListParticipants = require('./thread-list-participants');
 const ThreadListIcon = require('./thread-list-icon');
 
 // Get and format either last sent or last received timestamp depending on thread-list being viewed
-const ThreadListTimestamp = function ({ thread }) {
+const ThreadListTimestamp = function({ thread }) {
   // let rawTimestamp = FocusedPerspectiveStore.current().isSent()
   //   ? thread.lastMessageSentTimestamp
   //   : thread.lastMessageReceivedTimestamp;
@@ -29,7 +29,7 @@ const ThreadListTimestamp = function ({ thread }) {
 
 ThreadListTimestamp.containerRequired = false;
 
-const subject = function (subj) {
+const subject = function(subj) {
   if ((subj || '').trim().length === 0) {
     return <span className="no-subject">(No Subject)</span>;
   } else if (subj.split(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g).length > 1) {
@@ -53,7 +53,7 @@ const subject = function (subj) {
   }
 };
 
-const getSnippet = function (thread) {
+const getSnippet = function(thread) {
   const messages = thread.__messages || [];
   if (thread.snippet) {
     return thread.snippet;
@@ -186,7 +186,7 @@ const cNarrow = new ListTabular.Column({
     }
 
     const hasAttachments =
-      thread.attachmentCount > 0 && messages.find(m => Utils.showIconForAttachments(m.files));
+      thread.hasAttachments && messages.find(m => Utils.showIconForAttachments(m.files));
     if (hasAttachments) {
       attachment = <div className="thread-icon thread-icon-attachment" />;
     }
