0e77439599b4c43ab6e06ed25c611fb7f7e361ee
yangning <ning@edison.tech>
2020-04-21 16:11:55 +0800

    fix DC-1652: account sidebar height calculation error

--- app/internal_packages/edison-beijing-chat/lib/chat-view-left.jsx
+++ app/internal_packages/edison-beijing-chat/lib/chat-view-left.jsx
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import ConversationsPanel from '../components/conversations/ConversationsPanel';
 import ChatViewLeftResizeBar from '../components/common/ChatViewLeftResizeBar';
 import registerLoginChat from '../utils/register-login-chat';
+
+export const BOTTOM_OFFSET = 37;
+export const MIN_HEIGHT = 50;
+
 export default class ChatViewLeft extends Component {
   static displayName = 'ChatViewLeft';
 
@@ -12,7 +16,13 @@ export default class ChatViewLeft extends Component {
 
   render() {
     return (
-      <div className="chat-view-container chat-left-panel-container">
+      <div
+        className="chat-view-container chat-left-panel-container"
+        style={{
+          minHeight: MIN_HEIGHT + 'px',
+          bottom: BOTTOM_OFFSET + 'px',
+        }}
+      >
         <div className="left-panel">
           <ChatViewLeftResizeBar />
           <ConversationsPanel />
