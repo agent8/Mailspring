514cc8685abf26cd470756c575af85d790042b04
ruoxijiang <rjiang@lazycakestudios.net>
2020-04-30 22:05:01 +0800

    DC-1764 [Actions] Fail to delete the email which is moved to a new label

--- app/internal_packages/category-picker/lib/create-new-label-popover.jsx
+++ app/internal_packages/category-picker/lib/create-new-label-popover.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { RetinaImg, LabelColorizer } from 'mailspring-component-kit';
-import {
-  Actions,
-  SyncbackCategoryTask,
-  ChangeLabelsTask,
-  TaskQueue,
-} from 'mailspring-exports';
+import { Actions, SyncbackCategoryTask, ChangeLabelsTask, TaskQueue } from 'mailspring-exports';
 
 export default class CreateNewFolderPopover extends Component {
   static propTypes = {
@@ -20,7 +15,7 @@ export default class CreateNewFolderPopover extends Component {
   static defaultProps = {
     left: 490,
     top: 107,
-    buttonTimeout: 700,// timeout
+    buttonTimeout: 700, // timeout
   };
 
   constructor(props) {
@@ -29,7 +24,7 @@ export default class CreateNewFolderPopover extends Component {
       newName: this.props.name || '',
       alsoMove: true,
       isBusy: false,
-      bgColor: 0
+      bgColor: 0,
     };
     this._mounted = false;
     this._buttonTimer = null;
@@ -53,9 +48,14 @@ export default class CreateNewFolderPopover extends Component {
     }
   };
 
-  onBlur = (e) => {
+  onBlur = e => {
     const rect = this.container.getBoundingClientRect();
-    if (e.clientX < rect.left || e.clientX > rect.right || e.clientY < rect.top || e.clientY > rect.bottom) {
+    if (
+      e.clientX < rect.left ||
+      e.clientX > rect.right ||
+      e.clientY < rect.top ||
+      e.clientY > rect.bottom
+    ) {
       this.onCancel();
     }
   };
@@ -113,7 +113,10 @@ export default class CreateNewFolderPopover extends Component {
     Actions.queueTask(syncbackTask);
     TaskQueue.waitForPerformRemote(syncbackTask).then(finishedTask => {
       if (finishedTask.error) {
-        AppEnv.showErrorDialog({ title: 'Error', message: `Could not create label.${finishedTask.error && finishedTask.error.debuginfo}` });
+        AppEnv.showErrorDialog({
+          title: 'Error',
+          message: `Could not create label.${finishedTask.error && finishedTask.error.debuginfo}`,
+        });
         return;
       }
       if (this.props.isMoveAction) {
@@ -127,13 +130,13 @@ export default class CreateNewFolderPopover extends Component {
             labelsToAdd: [finishedTask.created],
           })
         );
-        this._onActionCallback({addedLabels: [finishedTask.created]});
+        this._onActionCallback({ addedLabels: [finishedTask.created] });
       }
     });
     Actions.closePopover();
   };
 
-  _onMoveToCategory = (category) => {
+  _onMoveToCategory = category => {
     // This is currently not used, replaced by moveFolder
     AppEnv.logError(`Should not have been used, use moveFolder flow instead`);
     const { threads } = this.props;
@@ -143,71 +146,91 @@ export default class CreateNewFolderPopover extends Component {
     Actions.queueTasks([
       new ChangeLabelsTask({
         source: 'Category Picker: Move to Label, deleting previous labels',
-        labelsToRemove: all,
+        labelsToRemove: all.filter(label => label.isLabel && label.isLabel()),
         labelsToAdd: [category],
         threads: threads,
-      })
+      }),
     ]);
     this._onActionCallback({ addedLabels: [category], removedLabels: all });
   };
 
-  _onNameChange = (e) => {
+  _onNameChange = e => {
     if (!this.state.isBusy) {
       this.setState({ newName: e.target.value });
     }
   };
 
-  onCheckColor = (bgColor) => {
+  onCheckColor = bgColor => {
     this.setState({
-      bgColor
-    })
-  }
+      bgColor,
+    });
+  };
 
   renderButtons() {
-    return <div className='button-row'>
-      <button className="create-folder-btn-cancel" title="Cancel" onClick={this.onCancel}>
-        <span>Cancel</span>
-      </button>
-      <button className="create-folder-btn-create" title="Create Folder"
-        disabled={this.state.newName.length === 0} onClick={this._onCreateCategory}>
-        {(this.state.isBusy || this._buttonTimer) ?
-          <RetinaImg name={'sending-spinner.gif'}
-            style={{ width: 24 }}
-            mode={RetinaImg.Mode.ContentIsMask} /> :
-          <span>Create Label</span>}
-      </button>
-    </div>;
+    return (
+      <div className="button-row">
+        <button className="create-folder-btn-cancel" title="Cancel" onClick={this.onCancel}>
+          <span>Cancel</span>
+        </button>
+        <button
+          className="create-folder-btn-create"
+          title="Create Folder"
+          disabled={this.state.newName.length === 0}
+          onClick={this._onCreateCategory}
+        >
+          {this.state.isBusy || this._buttonTimer ? (
+            <RetinaImg
+              name={'sending-spinner.gif'}
+              style={{ width: 24 }}
+              mode={RetinaImg.Mode.ContentIsMask}
+            />
+          ) : (
+            <span>Create Label</span>
+          )}
+        </button>
+      </div>
+    );
   }
 
   render() {
-    return <div ref={(el) => this.container = el}
-      className={`create-folder-container has-color-choice ${this.props.visible ? 'hide' : ''}`}>
-      <div className={'header-row'}>
-        <span className="close" onClick={this.onCancel}>
-          <RetinaImg
-            name="close_1.svg"
-            isIcon
-            mode={RetinaImg.Mode.ContentIsMask}
-            style={{ width: 20 }}
-          />
-        </span>
-      </div>
-      <div className='header-text-container'>
-        <div className='header-text'>New Label</div>
-        <div className='header-subtext'>What do you want to name it?</div>
-      </div>
-      <input className='folder-input'
-        value={this.state.newName} placeholder={'Name'}
-        disabled={this.state.isBusy}
-        onChange={this._onNameChange} />
-      <div>
-        <div className='header-subtext'>Pick a color for your new label:</div>
-        <div className="color-choice">
-          {
-            LabelColorizer.colors.map((color, idx) => {
+    return (
+      <div
+        ref={el => (this.container = el)}
+        className={`create-folder-container has-color-choice ${this.props.visible ? 'hide' : ''}`}
+      >
+        <div className={'header-row'}>
+          <span className="close" onClick={this.onCancel}>
+            <RetinaImg
+              name="close_1.svg"
+              isIcon
+              mode={RetinaImg.Mode.ContentIsMask}
+              style={{ width: 20 }}
+            />
+          </span>
+        </div>
+        <div className="header-text-container">
+          <div className="header-text">New Label</div>
+          <div className="header-subtext">What do you want to name it?</div>
+        </div>
+        <input
+          className="folder-input"
+          value={this.state.newName}
+          placeholder={'Name'}
+          disabled={this.state.isBusy}
+          onChange={this._onNameChange}
+        />
+        <div>
+          <div className="header-subtext">Pick a color for your new label:</div>
+          <div className="color-choice">
+            {LabelColorizer.colors.map((color, idx) => {
               const className = this.state.bgColor === idx ? 'checked' : '';
               return (
-                <div key={color} className={className} style={{ background: color }} onClick={() => this.onCheckColor(idx)} >
+                <div
+                  key={color}
+                  className={className}
+                  style={{ background: color }}
+                  onClick={() => this.onCheckColor(idx)}
+                >
                   <RetinaImg
                     className="check-img check"
                     name="tagging-checkmark.png"
@@ -215,12 +238,12 @@ export default class CreateNewFolderPopover extends Component {
                     onClick={() => this._onSelectLabel(item)}
                   />
                 </div>
-              )
-            })
-          }
+              );
+            })}
+          </div>
         </div>
+        {this.renderButtons()}
       </div>
-      {this.renderButtons()}
-    </div>;
+    );
   }
 }
