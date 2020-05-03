a157dc1b857ecdd7f6b87ed8e7a862d2bd3b1da7
quanzs <quanzhansheng@gmail.com>
2020-05-03 19:15:02 +0800

    use slate spellCheck instead of SpellcheckPlugins

--- app/src/components/composer-editor/composer-editor.jsx
+++ app/src/components/composer-editor/composer-editor.jsx
@@ -305,7 +305,7 @@ export default class ComposerEditor extends React.Component {
             onCut={this.onCut}
             onCopy={this.onCopy}
             onPaste={this.onPaste}
-            spellCheck={false}
+            spellCheck={true}
             readOnly={this.props.readOnly}
             plugins={plugins}
             propsForPlugins={propsForPlugins}
