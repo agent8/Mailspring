98bc2588993eeadde0fcf5056660f9c4290e29e8
yangning <ning@edison.tech>
2020-05-12 10:49:39 +0800

    add transform svg to icon font

---
+++ scripts/generate-icon-font.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var iconfont = require('gulp-iconfont');
+var iconfontCss = require('gulp-iconfont-css');
+var path = require('path');
+var absPath = path.resolve('../');
+var fontName = 'EdisonIcons';
+var dealPath = path.join(absPath, './app/static/icons/');
+
+gulp.task('Iconfont', function() {
+  return gulp
+    .src([path.join(dealPath, '*.svg')])
+    .pipe(
+      iconfontCss({
+        fontName: fontName,
+        path: path.join(dealPath, 'template.css'),
+        cssClass: 'edison-icon',
+        targetPath: '../edison-icon.css',
+        fontPath: './fonts/',
+      })
+    )
+    .pipe(
+      iconfont({
+        fontName: fontName,
+        normalize: true,
+        centerHorizontally: true,
+      })
+    )
+    .pipe(gulp.dest(path.join(absPath, './app/static/fonts')));
+});
