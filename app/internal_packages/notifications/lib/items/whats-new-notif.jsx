9487bfda88515ec5c3d20d6d81aeffccd3369d19
yangning <ning@edison.tech>
2020-06-02 14:07:40 +0800

    what's new page

--- app/internal_packages/notifications/lib/items/whats-new-notif.jsx
+++ app/internal_packages/notifications/lib/items/whats-new-notif.jsx
@@ -1,15 +1,18 @@
-import { React, RESTful } from 'mailspring-exports';
+import { React, RESTful, Constant } from 'mailspring-exports';
 import { FullScreenModal, RetinaImg } from 'mailspring-component-kit';
+import { ipcRenderer } from 'electron';
 
-const { AppUpdate } = RESTful;
+const { UserReviewUrl } = Constant;
+const { AppUpdateRest } = RESTful;
+
 export default class WhatsNew extends React.Component {
   static displayName = 'WhatsNew';
 
   constructor() {
     super();
     this.state = {
-      showUserReview: true,
-      pageIdx: 0,
+      showUserReview: false,
+      finished: true,
       pages: [
         {
           image: 'whatsnew-rating.png',
@@ -21,23 +24,53 @@ export default class WhatsNew extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this._getUpdateInformation();
+  }
+
+  _getUpdateInformation = async () => {
+    const result = await AppUpdateRest.getUpdateInformation();
+    const updateList = [];
+    try {
+      const data = JSON.parse(result.data);
+      if (data && data.updateList && data.updateList.length) {
+        updateList.push(...data.updateList);
+      }
+    } catch (e) {}
+
+    this.setState({
+      showUserReview: true,
+      finished: updateList.length <= 0,
+      pages: [...updateList, ...this.state.pages],
+    });
+  };
+
   _onCloseUserReview = () => {
     this.setState({
       showUserReview: false,
     });
   };
 
+  _onNext = () => {
+    this.setState({
+      finished: true,
+    });
+  };
+
+  _onClickReview = () => {
+    ipcRenderer.send('open-url', UserReviewUrl);
+  };
+
   render() {
-    const { showUserReview, pageIdx, pages } = this.state;
-    const nowPage = pages[pageIdx];
-    const nowIsLastPage = pageIdx === pages.length - 1;
+    const { showUserReview, finished, pages } = this.state;
+    const nowPage = pages[0];
     return (
       <FullScreenModal
         visible={showUserReview}
         onCancel={this._onCloseUserReview}
         style={{ height: '500px', width: '600px' }}
         mask
-        closable={nowIsLastPage}
+        closable={finished}
       >
         <div className="focused-inbox-notif">
           <RetinaImg
@@ -48,12 +81,12 @@ export default class WhatsNew extends React.Component {
           <h2>{nowPage.title}</h2>
           <p>{nowPage.message}</p>
           <div className="btn-list">
-            {nowIsLastPage ? (
-              <div className="btn modal-btn-enable" onClick={() => this._onClickButton(true)}>
+            {finished ? (
+              <div className="btn modal-btn-enable" onClick={() => this._onClickReview()}>
                 Leave a Review
               </div>
             ) : (
-              <div className="btn modal-btn-enable" onClick={() => this._onClickButton(true)}>
+              <div className="btn modal-btn-enable" onClick={() => this._onNext()}>
                 Next
               </div>
             )}
