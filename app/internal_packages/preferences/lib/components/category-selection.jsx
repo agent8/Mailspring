078a1b7b6adf08b337a6b9dbac7552521f45072f
quanzs <quanzhansheng@gmail.com>
2020-07-17 18:51:46 +0800

    DC-2220 [Feedback] nothing shows up but an blank Inbox

--- app/internal_packages/preferences/lib/components/category-selection.jsx
+++ app/internal_packages/preferences/lib/components/category-selection.jsx
@@ -29,8 +29,10 @@ export default class CategorySelection extends React.Component {
     }
     return this.props.all
       .sort((a, b) => {
-        var pathA = utf7.imap.decode(a.name || a.path).toUpperCase();
-        var pathB = utf7.imap.decode(b.name || b.path).toUpperCase();
+        // var pathA = utf7.imap.decode(a.name || a.path).toUpperCase();
+        // var pathB = utf7.imap.decode(b.name || b.path).toUpperCase();
+        var pathA = (a.name || utf7.imap.decode(a.path)).toUpperCase();
+        var pathB = (b.name || utf7.imap.decode(b.path)).toUpperCase();
         if (pathA < pathB) {
           return -1;
         }
@@ -39,7 +41,10 @@ export default class CategorySelection extends React.Component {
         }
         return 0;
       })
-      .filter(c => Utils.wordSearchRegExp(this.state.searchValue).test(utf7.imap.decode(c.name || c.path)))
+      .filter(c =>
+        // Utils.wordSearchRegExp(this.state.searchValue).test(utf7.imap.decode(c.name || c.path))
+        Utils.wordSearchRegExp(this.state.searchValue).test(c.name || utf7.imap.decode(c.path))
+      )
       .map(c => {
         c.backgroundColor = LabelColorizer.backgroundColorDark(c);
         return c;
@@ -65,7 +70,7 @@ export default class CategorySelection extends React.Component {
       );
     }
 
-    const displayPath = utf7.imap.decode(item.name || item.path);
+    const displayPath = item.name || utf7.imap.decode(item.path);
 
     return (
       <div className="category-item">
