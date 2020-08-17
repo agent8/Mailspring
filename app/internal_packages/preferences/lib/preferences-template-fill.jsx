e70b7a2b36478dd05363eaf4b817cc2eff49fd79
deming16 <deminglindm@gmail.com>
2020-08-17 17:00:22 +0800

    test i18n

--- app/internal_packages/preferences/lib/preferences-template-fill.jsx
+++ app/internal_packages/preferences/lib/preferences-template-fill.jsx
@@ -30,12 +30,41 @@ import {
   PreferencesKeymapsHearder,
   PreferencesKeymapsContent,
 } from './components/preferences-keymaps';
+import i18n from 'i18next';
+import translationEN from './translationEN.json';
+import translationDE from './translationDE.json';
+i18n
+  .init({
+    // we init with resources
+    resources: {
+      en: {
+        translations: translationEN
+      },
+      de: {
+        translations: translationDE
+      }
+    },
+    fallbackLng: "en",
+    debug: true,
+
+    // have a common namespace used around the full app
+    ns: ["translations"],
+    defaultNS: "translations",
+
+    keySeparator: false, // we use content as keys
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+// change language
+// i18n.changeLanguage('de');
 
 const preferencesTemplateFill = {
   tables: [
     {
       tabId: 'General',
-      displayName: 'General',
+      displayName: i18n.t('General'),
       className: 'container-general',
       order: 1,
       configGroup: [
