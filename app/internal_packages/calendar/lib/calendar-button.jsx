343a3a88b4591d58efed6c247de7d151ff1e3830
deming16 <deminglindm@gmail.com>
2020-10-05 14:32:26 +0800

    Created events are created multiple times

--- app/internal_packages/calendar/lib/calendar-button.jsx
+++ app/internal_packages/calendar/lib/calendar-button.jsx
@@ -1,44 +1,44 @@
-import { ComponentRegistry, WorkspaceStore, React } from 'mailspring-exports';
-const { ResizableRegion, RetinaImg } = require('mailspring-component-kit');
-import IntegratedCalendar from './mailprep/app/IntegratedCalendar';
+// import { ComponentRegistry, WorkspaceStore, React } from 'mailspring-exports';
+// const { ResizableRegion, RetinaImg } = require('mailspring-component-kit');
+// import IntegratedCalendar from './mailprep/app/IntegratedCalendar';
 
-export default class CalendarButton extends React.Component {
-  static displayName = 'CalendarButton';
+// export default class CalendarButton extends React.Component {
+//   static displayName = 'CalendarButton';
 
-  shouldComponentUpdate(nextProps) {
-    // Our render method doesn't use the provided `draft`, and the draft changes
-    // constantly (on every keystroke!) `shouldComponentUpdate` helps keep Mailspring fast.
-    return nextProps.session !== this.props.session;
-  }
+//   shouldComponentUpdate(nextProps) {
+//     // Our render method doesn't use the provided `draft`, and the draft changes
+//     // constantly (on every keystroke!) `shouldComponentUpdate` helps keep Mailspring fast.
+//     return nextProps.session !== this.props.session;
+//   }
 
-  _onClick = () => {
-    // AppEnv.newWindow({
-    //   title: '',
-    //   hidden: true,
-    //   windowKey: `calendar-plugin`,
-    //   windowType: 'calendar-plugin',
-    //   windowLevel: 1,
-    // });
-    ComponentRegistry.register(IntegratedCalendar, {
-      role: 'plugins'
-    });
-  };
+//   _onClick = () => {
+//     // AppEnv.newWindow({
+//     //   title: '',
+//     //   hidden: true,
+//     //   windowKey: `calendar-plugin`,
+//     //   windowType: 'calendar-plugin',
+//     //   windowLevel: 1,
+//     // });
+//     ComponentRegistry.register(IntegratedCalendar, {
+//       role: 'plugins'
+//     });
+//   };
 
-  _getDialog() {
-    return require('electron').remote.dialog;
-  }
+//   _getDialog() {
+//     return require('electron').remote.dialog;
+//   }
 
-  render() {
-    return (
-      <div className="button-group" style={{ order: -1 }}>
-        <div
-          className={`btn-toolbar message-toolbar-jira`}
-          onClick={() => this._onClick()}
-        >
-          calendar
-        </div>
-      </div>
+//   render() {
+//     return (
+//       <div className="button-group" style={{ order: -1 }}>
+//         <div
+//           className={`btn-toolbar message-toolbar-jira`}
+//           onClick={() => this._onClick()}
+//         >
+//           calendar
+//         </div>
+//       </div>
 
-    );
-  }
-}
+//     );
+//   }
+// }
