d924c67b2c75f7204adbe93061468d18746aa8dd
deming16 <deminglindm@gmail.com>
2020-10-29 11:09:29 +0800

    modify config dir for sqlite

--- app/internal_packages/calendar/lib/mailprep/app/sequelizeDB/index.jsx
+++ app/internal_packages/calendar/lib/mailprep/app/sequelizeDB/index.jsx
@@ -18,7 +18,7 @@ export const getdb = async () => {
   // const configDirPath = AppEnv.getConfigDirPath();
   // const dbPath = path.join(configDirPath, 'calendar-db');
 
-  const configDirPath = '../';
+  const configDirPath = './app/internal_packages/calendar/lib/mailprep/app';
   const dbPath = path.join(configDirPath, 'calendar-db');
   if (!fs.existsSync(dbPath)) {
     fs.mkdirSync(dbPath);
@@ -27,8 +27,8 @@ export const getdb = async () => {
   db = new Sequelize({
     dialect: 'sqlite',
     storage: `calendar-db.sqlite`,
-    logging: false
-    // storage: `${dbPath}/calendar-db.sqlite`
+    logging: false,
+    storage: `${dbPath}/calendar-db.sqlite`
   });
 
   // #region Users Init
