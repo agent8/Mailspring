95e66b91f77c95ad364a6cb9c1065b11ccc6f9e3
ruoxijiang <rjiang@lazycakestudios.net>
2020-12-25 16:36:51 +0800

    DC-2634 show Search bar and category button of emoji

--- app/src/components/composer-editor/emoji-plugins.jsx
+++ app/src/components/composer-editor/emoji-plugins.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import NodeEmoji from 'node-emoji';
 import { Actions } from 'mailspring-exports';
 
@@ -49,6 +50,9 @@ function ImageBasedEmoji(props) {
     />
   );
 }
+ImageBasedEmoji.propTypes = {
+  name: PropTypes.string,
+};
 
 /* React Component that positions itself beneath the [data-emoji-typing] node
 within the RichEditor. Note that this positioning logic fails on first render
@@ -121,7 +125,10 @@ function FloatingEmojiPicker({ value, onChange }) {
     </div>
   );
 }
-
+FloatingEmojiPicker.propTypes = {
+  value: PropTypes.object,
+  onChange: PropTypes.func,
+};
 function renderNode({ node, attributes, children, targetIsHTML }) {
   if (node.type === EMOJI_TYPE) {
     const name = node.data.name || node.data.get('name');
@@ -198,68 +205,68 @@ export function updateEmojiMark(change, emoji, { typed, suggestions, picked }) {
   return change;
 }
 
-function onKeyDown(event, change, editor) {
-  if ([' ', 'Return', 'Enter'].includes(event.key)) {
-    const emoji = change.value.marks.find(i => i.type === EMOJI_TYPING_TYPE);
-    if (!emoji) return;
-    const picked = emoji.data.get('picked');
-    swapEmojiMarkFor(change, emoji, picked);
-    if (picked) {
-      event.preventDefault();
-      return true;
-    }
-  } else if (['ArrowDown', 'ArrowUp'].includes(event.key)) {
-    const dir = event.key.includes('Down') ? 1 : -1;
-    const emoji = change.value.marks.find(i => i.type === EMOJI_TYPING_TYPE);
-    if (!emoji) return;
-    const suggestions = emoji.data.get('suggestions');
-    const typed = emoji.data.get('typed');
-    let picked = emoji.data.get('picked');
-    const len = suggestions.length;
-    picked = suggestions[(len + suggestions.indexOf(picked) + dir) % len];
-    updateEmojiMark(change, emoji, { typed, suggestions, picked });
-    event.preventDefault();
-    return true;
-  }
-}
-
-function onKeyUp(event, change, editor) {
-  const emoji = change.value.marks.find(i => i.type === EMOJI_TYPING_TYPE);
-  if (!emoji) {
-    const justTyped = change.value.change().extend(-1).value.fragment.text;
-    if (justTyped.trim() === ':') {
-      change.addMark({ type: EMOJI_TYPING_TYPE, data: { typed: '', suggestions: [], picked: '' } });
-    }
-    return;
-  }
-
-  if (event.key.length === 1 || event.key === 'Backspace' || event.key === 'Delete') {
-    let typed = '';
-    const tmp = change.value.change();
-    while (true) {
-      tmp.extend(-1);
-      if (typed === tmp.value.fragment.text) {
-        break;
-      }
-      typed = tmp.value.fragment.text;
-      if (typed.startsWith(':')) {
-        break;
-      }
-    }
-    if (typed.length > 10 || !typed.startsWith(':')) {
-      change.removeMark(emoji);
-      return;
-    }
-    let suggestions = [];
-    let picked = emoji.data.get('picked');
-    if (typed.length >= 3) {
-      suggestions = getEmojiSuggestions(typed.replace(':', ''));
-      const pickedIdx = suggestions.indexOf(picked);
-      picked = suggestions[pickedIdx === -1 ? 0 : pickedIdx];
-    }
-    updateEmojiMark(change, emoji, { typed, suggestions, picked });
-  }
-}
+// function onKeyDown(event, change, editor) {
+//   if ([' ', 'Return', 'Enter'].includes(event.key)) {
+//     const emoji = change.value.marks.find(i => i.type === EMOJI_TYPING_TYPE);
+//     if (!emoji) return;
+//     const picked = emoji.data.get('picked');
+//     swapEmojiMarkFor(change, emoji, picked);
+//     if (picked) {
+//       event.preventDefault();
+//       return true;
+//     }
+//   } else if (['ArrowDown', 'ArrowUp'].includes(event.key)) {
+//     const dir = event.key.includes('Down') ? 1 : -1;
+//     const emoji = change.value.marks.find(i => i.type === EMOJI_TYPING_TYPE);
+//     if (!emoji) return;
+//     const suggestions = emoji.data.get('suggestions');
+//     const typed = emoji.data.get('typed');
+//     let picked = emoji.data.get('picked');
+//     const len = suggestions.length;
+//     picked = suggestions[(len + suggestions.indexOf(picked) + dir) % len];
+//     updateEmojiMark(change, emoji, { typed, suggestions, picked });
+//     event.preventDefault();
+//     return true;
+//   }
+// }
+//
+// function onKeyUp(event, change, editor) {
+//   const emoji = change.value.marks.find(i => i.type === EMOJI_TYPING_TYPE);
+//   if (!emoji) {
+//     const justTyped = change.value.change().extend(-1).value.fragment.text;
+//     if (justTyped.trim() === ':') {
+//       change.addMark({ type: EMOJI_TYPING_TYPE, data: { typed: '', suggestions: [], picked: '' } });
+//     }
+//     return;
+//   }
+//
+//   if (event.key.length === 1 || event.key === 'Backspace' || event.key === 'Delete') {
+//     let typed = '';
+//     const tmp = change.value.change();
+//     while (true) {
+//       tmp.extend(-1);
+//       if (typed === tmp.value.fragment.text) {
+//         break;
+//       }
+//       typed = tmp.value.fragment.text;
+//       if (typed.startsWith(':')) {
+//         break;
+//       }
+//     }
+//     if (typed.length > 10 || !typed.startsWith(':')) {
+//       change.removeMark(emoji);
+//       return;
+//     }
+//     let suggestions = [];
+//     let picked = emoji.data.get('picked');
+//     if (typed.length >= 3) {
+//       suggestions = getEmojiSuggestions(typed.replace(':', ''));
+//       const pickedIdx = suggestions.indexOf(picked);
+//       picked = suggestions[pickedIdx === -1 ? 0 : pickedIdx];
+//     }
+//     updateEmojiMark(change, emoji, { typed, suggestions, picked });
+//   }
+// }
 
 const ToolbarEmojiButton = ({ value, onChange, anchorEl = null, closePopover = null }) => {
   const onInsertEmoji = name => {
@@ -288,13 +295,13 @@ const ToolbarEmojiButton = ({ value, onChange, anchorEl = null, closePopover = n
       setTimeout(() => {
         Actions.openPopover(<EmojiToolbarPopover onInsertEmoji={onInsertEmoji} />, {
           originRect: originRect,
-          direction: 'up',
+          direction: 'down',
         });
       });
     } else {
       Actions.openPopover(<EmojiToolbarPopover onInsertEmoji={onInsertEmoji} />, {
         originRect: originRect,
-        direction: 'up',
+        direction: 'down',
       });
     }
   };
@@ -313,6 +320,12 @@ const ToolbarEmojiButton = ({ value, onChange, anchorEl = null, closePopover = n
       <i className="dt-icon dt-icon-emoji" />
     </button>
   );
+};
+ToolbarEmojiButton.propTypes = {
+  value: PropTypes.object,
+  onChange: PropTypes.func,
+  anchorEl: PropTypes.element,
+  closePopover: PropTypes.bool,
 };
 
 export default [
