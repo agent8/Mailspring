fb46ef9c08acd3726574f894b54dc6fe93c1879b
quanzs <quanzhansheng@gmail.com>
2021-01-12 16:38:55 +0800

    encrypt s3 secret

--- scripts/upload_dmg.js
+++ scripts/upload_dmg.js
@@ -1,10 +1,18 @@
 var AWS = require('aws-sdk');
 const { version } = require('../app/package.json');
 const BUCKET = 'edison-static2';
+
+function decrypt(ciphertext) {
+  const CryptoJS = require('crypto-js');
+  const E_KEY = 'EDISON_MAIL';
+  var bytes = CryptoJS.AES.decrypt(ciphertext, E_KEY);
+  return bytes.toString(CryptoJS.enc.Utf8);
+}
+
 let s3options = {
   region: process.env.S3_REGION || 'ENV_S3_REGION',
-  accessKeyId: process.env.S3_ACCESSKEY_ID || 'ENV_S3_ACCESSKEY_ID',
-  secretAccessKey: process.env.S3_SECRET_ACCESSKEY_FOR_STATIC || 'ENV_S3_SECRET_ACCESSKEY',
+  accessKeyId: decrypt(process.env.S3_ACCESSKEY_ID || 'ENV_S3_ACCESSKEY_ID'),
+  secretAccessKey: decrypt(process.env.S3_SECRET_ACCESSKEY_FOR_STATIC || 'ENV_S3_SECRET_ACCESSKEY'),
   Endpoint: 'http://s3.us-east-2.amazonaws.com',
 };
 // Set the region
