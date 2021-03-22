eeff6a907703420fb17cd395ed67289576adf6b8
ruoxijiang <rjiang@lazycakestudios.net>
2021-03-22 14:50:57 +0800

    DC-3030 show failed reason in outbox

--- app/internal_packages/outbox-message/lib/main.jsx
+++ app/internal_packages/outbox-message/lib/main.jsx
@@ -1,10 +1,14 @@
 import { ComponentRegistry } from 'mailspring-exports';
 import { OutboxMessageButtons } from './outbox-toolbar-buttons';
+import OutboxMessageFailedReason from './outbox-message-failed-reason';
 
 export function activate() {
   if (AppEnv.isMainWindow()) {
     ComponentRegistry.register(OutboxMessageButtons, {
       role: 'OutboxMessageToolbar',
+    });
+    ComponentRegistry.register(OutboxMessageFailedReason, {
+      role: 'message:BodyHeader',
     });
   }
 }
